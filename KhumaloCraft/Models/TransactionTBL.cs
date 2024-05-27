47ad40fc070f6c303647e7e4b8148b745402e633
Jose Lubota <133894595+JoseLubota@users.noreply.github.com>
2024-05-27 17:29:04 +0200

    part 2

--- KhumaloCraft/Models/TransactionTBL.cs
+++ KhumaloCraft/Models/TransactionTBL.cs
@@ -19,40 +19,73 @@ namespace KhumaloCraft.Models
 
         public static List<TransactionTBL> getUserDetails(int userID)
         {
-            List<TransactionTBL> orderDetails = new List<TransactionTBL>();
-            string con_string = "Server=tcp:clvd-sql-server.database.windows.net,1433;Initial Catalog=clvd-db;Persist Security Info=False;User ID=Jose;Password=2004Fr@ney;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
-            using (SqlConnection con = new SqlConnection(con_string))
+            if(userID != 1)
             {
-                string sql = "SELECT TOP 1 TransactionTable.orderID, userTBL.userName,  userTBL.userID, userTBL.surname, userTBL.email, productTBL.productName, productTBL.productPrice,  productTBL.productCategory\r\nFROM TransactionTable\r\nINNER JOIN userTBL ON TransactionTable.userID = userTBL.userID\r\nINNER JOIN productTBL ON TransactionTable.productID = productTBL.productID\r\nWHERE userTBL.userID = @userID";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.Parameters.AddWithValue("@userID", userID);
-                con.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                List<TransactionTBL> orderDetails = new List<TransactionTBL>();
+                string con_string = "Server=tcp:clvd-sql-server.database.windows.net,1433;Initial Catalog=clvd-db;Persist Security Info=False;User ID=Jose;Password=2004Fr@ney;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+                using (SqlConnection con = new SqlConnection(con_string))
                 {
-                    TransactionTBL order = new TransactionTBL();
-                    order.orderID = Convert.ToInt32(reader["orderID"]);
-                    if (reader["userID"] == null)
+                    string sql = "SELECT TOP 1 TransactionTable.orderID, userTBL.userName,  userTBL.userID, userTBL.surname, userTBL.email, productTBL.productName, productTBL.productPrice,  productTBL.productCategory\r\nFROM TransactionTable\r\nINNER JOIN userTBL ON TransactionTable.userID = userTBL.userID\r\nINNER JOIN productTBL ON TransactionTable.productID = productTBL.productID\r\nWHERE userTBL.userID = @userID";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.Parameters.AddWithValue("@userID", userID);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
                     {
-                        order.userID = 0;
-                    }
-                    else
-                    {
-                        order.userID = Convert.ToInt32(reader["userID"]);
-                    }
+                        TransactionTBL order = new TransactionTBL();
+                        order.orderID = Convert.ToInt32(reader["orderID"]);
+                        if (reader["userID"] == null)
+                        {
+                            order.userID = 0;
+                        }
+                        else
+                        {
+                            order.userID = Convert.ToInt32(reader["userID"]);
+                        }
 
-                   
-                    order.userName = Convert.ToString(reader["userName"]);
-                    order.surname = Convert.ToString(reader["surname"]);
-                    order.productName = Convert.ToString(reader["productName"]);
-                    order.productPrice = Convert.ToDecimal(reader["productPrice"]);
-                    order.productCategory = Convert.ToString(reader["productCategory"]);
 
-                    orderDetails.Add(order);
+                        order.userName = Convert.ToString(reader["userName"]);
+                        order.surname = Convert.ToString(reader["surname"]);
+                        order.productName = Convert.ToString(reader["productName"]);
+                        order.productPrice = Convert.ToDecimal(reader["productPrice"]);
+                        order.productCategory = Convert.ToString(reader["productCategory"]);
+
+                        orderDetails.Add(order);
+                    }
+                    reader.Close();
                 }
-                reader.Close();
+                return orderDetails;
+
             }
-            return orderDetails;
+            else
+            {
+                List<TransactionTBL> orderDetails = new List<TransactionTBL>();
+                string con_string = "Server=tcp:clvd-sql-server.database.windows.net,1433;Initial Catalog=clvd-db;Persist Security Info=False;User ID=Jose;Password=2004Fr@ney;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+                using (SqlConnection con = new SqlConnection(con_string))
+                {
+                    string sql = "SELECT  TransactionTable.orderID, userTBL.userID, userTBL.userName, userTBL.surname, userTBL.email, productTBL.productName, productTBL.productPrice,  productTBL.productCategory\r\nFROM TransactionTable\r\nINNER JOIN userTBL ON TransactionTable.userID = userTBL.userID\r\nINNER JOIN productTBL ON TransactionTable.productID = productTBL.productID;";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        TransactionTBL order = new TransactionTBL();
+                        order.orderID = Convert.ToInt32(reader["orderID"]);
+                        order.userID = Convert.ToInt32(reader["userID"]);
+                        order.userName = Convert.ToString(reader["userName"]);
+                        order.surname = Convert.ToString(reader["surname"]);
+                        order.productName = Convert.ToString(reader["productName"]);
+                        order.productPrice = Convert.ToDecimal(reader["productPrice"]);
+                        order.productCategory = Convert.ToString(reader["productCategory"]);
+                        orderDetails.Add(order);
+                    }
+                    reader.Close();
+                }
+                return orderDetails;
+
+
+            }
+
 
         }
         public static List<TransactionTBL> getProductDetails(int userID)
@@ -82,5 +115,7 @@ namespace KhumaloCraft.Models
             return orderDetails;
 
         }
+
+
     }
 }
